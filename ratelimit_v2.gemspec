9ff8e11c6cff33aed93bc05abcaf88ddb050c9cb
Mathusan Selvarajah <mathusans52@gmail.com>
2021-09-17 12:10:41 -0400

    Initial release (#1)

--- ratelimit_v2.gemspec
+++ ratelimit_v2.gemspec
@@ -1,29 +1,39 @@
 require_relative 'lib/ratelimit_v2/version'
 
 Gem::Specification.new do |spec|
-  spec.name          = "ratelimit_v2"
+  spec.name          = 'ratelimit_v2'
   spec.version       = RatelimitV2::VERSION
-  spec.authors       = ["Mathusan Selvarajah"]
-  spec.email         = ["mathusans52@gmail.com"]
+  spec.authors       = ['Mathusan Selvarajah']
+  spec.email         = ['mathusans52@gmail.com']
 
-  spec.summary       = %q{TODO: Write a short summary, because RubyGems requires one.}
-  spec.description   = %q{TODO: Write a longer description or delete this line.}
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
-  spec.license       = "MIT"
-  spec.required_ruby_version = Gem::Requirement.new(">= 2.3.0")
+  spec.summary       = 'A redis backed rate limiter.'
+  spec.description   = 'A redis backed rate limiter. Appropriate for use cases where in-memory rate limiting \
+                          would not work (i.e rate limiting across multiple processes, servers, apps, etc).'
+  spec.homepage      = 'https://github.com/koffeefinance/ratelimit_v2'
+  spec.license       = 'MIT'
+  spec.required_ruby_version = Gem::Requirement.new('>= 2.7.0')
 
-  spec.metadata["allowed_push_host"] = "TODO: Set to 'http://mygemserver.com'"
+  spec.metadata['allowed_push_host'] = 'https://rubygems.org'
 
-  spec.metadata["homepage_uri"] = spec.homepage
-  spec.metadata["source_code_uri"] = "TODO: Put your gem's public repo URL here."
-  spec.metadata["changelog_uri"] = "TODO: Put your gem's CHANGELOG.md URL here."
+  spec.metadata['homepage_uri'] = spec.homepage
+  spec.metadata['source_code_uri'] = 'https://github.com/koffeefinance/ratelimit_v2'
+  spec.metadata['changelog_uri'] = 'https://github.com/koffeefinance/ratelimit_v2/CHANGELOG.md'
 
   # Specify which files should be added to the gem when it is released.
   # The `git ls-files -z` loads the files in the RubyGem that have been added into git.
-  spec.files         = Dir.chdir(File.expand_path('..', __FILE__)) do
+  spec.files = Dir.chdir(File.expand_path(__dir__)) do
     `git ls-files -z`.split("\x0").reject { |f| f.match(%r{^(test|spec|features)/}) }
   end
-  spec.bindir        = "exe"
+  spec.bindir        = 'exe'
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
-  spec.require_paths = ["lib"]
+  spec.require_paths = ['lib']
+
+  spec.add_dependency             'redis', '~> 4.4'
+  spec.add_dependency             'redis-namespace', '~> 1.8.1'
+
+  spec.add_development_dependency 'fakeredis', '~> 0.8.0'
+  spec.add_development_dependency 'minitest', '~> 5.0'
+  spec.add_development_dependency 'mocha', '~> 1.13'
+  spec.add_development_dependency 'rake', '~> 12.0'
+  spec.add_development_dependency 'timecop', '~> 0.9.4'
 end
--- test/ratelimit_v2_test.rb
+++ test/ratelimit_v2_test.rb
@@ -1,11 +1,121 @@
-require "test_helper"
+require 'test_helper'
+require 'redis'
 
 class RatelimitV2Test < Minitest::Test
+  def setup
+    @limiter = RatelimitV2.new(
+      name: 'test',
+      threshold: 10,
+      interval: 5,
+      time_span: 600
+    )
+
+    # will connect to the fakeredis that the limiter is connected to
+    @redis = Redis.new
+    @redis.flushdb
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::RatelimitV2::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_argument_error_raised_if_interval_greater_than_time_span
+    assert_raises ArgumentError do
+      RatelimitV2.new(
+        name: 'test',
+        threshold: 10,
+        interval: 800,
+        time_span: 600
+      )
+    end
+  end
+
+  def test_add_initializes_count_to_one
+    stub_time
+
+    @limiter.add
+    assert_equal 1, @redis.get('limiter:test:22').to_i
+  end
+
+  def test_add_increments_count_by_one
+    stub_time
+
+    @limiter.add
+    assert_equal 1, @redis.get('limiter:test:22').to_i
+
+    @limiter.add
+    assert_equal 2, @redis.get('limiter:test:22').to_i
+  end
+
+  def test_count_retuns_sum_across_all_buckets
+    10.times { @limiter.add }
+
+    assert_equal 10, @limiter.count
+  end
+
+  def test_exceed_returns_false_when_count_less_than_threshold
+    9.times { @limiter.add }
+
+    assert !@limiter.exceeded?
+  end
+
+  def test_exceed_returns_true_when_count_greater_than_threshold
+    11.times { @limiter.add }
+
+    assert @limiter.exceeded?
+  end
+
+  # NOTE: this is sort of an integration test rather than a unit test
+  def test_exec_within_threshold_waits_when_threshold_hit
+    limiter = RatelimitV2.new(
+      name: 'test',
+      threshold: 10,
+      interval: 4,
+      bucket_span: 4,
+      time_span: 600
+    )
+
+    count = 0
+
+    # verifies sleep is actually called
+    assert_raises Timeout::Error do
+      Timeout.timeout(3) do
+        12.times do
+          limiter.add
+          limiter.exec_within_threshold do
+            count += 1
+          end
+        end
+      end
+    end
+
+    count = 0
+
+    # fast forward 15 seconds where rate limits should be cleared
+    Timecop.travel(15) do
+      assert !limiter.exceeded?
+      # since rate limit is 10 operations per 4 seconds, 12 operations should finish in 5 seconds
+      Timeout.timeout(5) do
+        12.times do
+          limiter.add
+          limiter.exec_within_threshold do
+            count += 1
+          end
+        end
+      end
+    end
+
+    assert_equal 12, count
+  end
+
+  def test_bucket_index_returns_expected_index
+    stub_time
+
+    assert_equal 22, @limiter.send(:bucket_index)
+  end
+
+  def stub_time
+    test_time = 1_631_816_992
+    Time.stubs(:now).returns(Time.at(test_time))
   end
 end

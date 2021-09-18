df007ade11a6395ee74d6e2785c610e28117e651
Mathusan Selvarajah <mathusans52@gmail.com>
2021-09-18 12:48:08 -0400

    Simplify ratelimitcop init, add execute method, add dummyAPI class for test

---
+++ lib/errors.rb
@@ -0,0 +1,1 @@
+class InvalidBucketConfigError < StandardError; end
--- test/ratelimitcop_test.rb
+++ test/ratelimitcop_test.rb
@@ -6,8 +6,7 @@ class RatelimitcopTest < Minitest::Test
     @limiter = Ratelimitcop.new(
       name: 'test',
       threshold: 10,
-      interval: 5,
-      time_span: 600
+      interval: 5
     )
 
     # will connect to the fakeredis that the limiter is connected to
@@ -15,17 +14,63 @@ class RatelimitcopTest < Minitest::Test
     @redis.flushdb
   end
 
+  def after
+    Timecop.return
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::Ratelimitcop::VERSION
   end
 
-  def test_argument_error_raised_if_interval_greater_than_time_span
-    assert_raises ArgumentError do
+  def test_bucket_interval_option_is_set
+    limiter = Ratelimitcop.new(
+      name: 'test',
+      threshold: 10,
+      interval: 5,
+      options: {
+        bucket_interval: 6
+      }
+    )
+
+    assert_equal 6, limiter.instance_variable_get(:@bucket_interval)
+  end
+
+  def test_bucket_time_span_option_is_set
+    limiter = Ratelimitcop.new(
+      name: 'test',
+      threshold: 10,
+      interval: 5,
+      options: {
+        bucket_time_span: 700
+      }
+    )
+
+    assert_equal 700, limiter.instance_variable_get(:@bucket_time_span)
+  end
+
+  def test_bucket_span_option_is_set
+    limiter = Ratelimitcop.new(
+      name: 'test',
+      threshold: 10,
+      interval: 5,
+      options: {
+        bucket_span: 4
+      }
+    )
+
+    assert_equal 4, limiter.instance_variable_get(:@bucket_span)
+  end
+
+  def test_invalid_bucket_config_error_raised_if_bucket_interval_greater_than_bucket_time_span
+    assert_raises InvalidBucketConfigError do
       Ratelimitcop.new(
         name: 'test',
         threshold: 10,
         interval: 800,
-        time_span: 600
+        options: {
+          bucket_interval: 800,
+          bucket_time_span: 600
+        }
       )
     end
   end
@@ -70,9 +115,7 @@ class RatelimitcopTest < Minitest::Test
     limiter = Ratelimitcop.new(
       name: 'test',
       threshold: 10,
-      interval: 4,
-      bucket_span: 4,
-      time_span: 600
+      interval: 4
     )
 
     count = 0
@@ -112,6 +155,30 @@ class RatelimitcopTest < Minitest::Test
     stub_time
 
     assert_equal 22, @limiter.send(:bucket_index)
+  end
+
+  def test_exec_handles_dummy_api_rate_limit
+    limiter = Ratelimitcop.new(
+      name: 'test_exec_handles_dummy_api_rate_limit',
+      threshold: 10,
+      interval: 2
+    )
+
+    dummy_api = DummyAPI.new(
+      rate_limit_threshold: 10,
+      rate_limit_interval: 2
+    )
+
+    count = 0
+
+    15.times do
+      limiter.execute do
+        dummy_api.request
+        count += 1
+      end
+    end
+
+    assert_equal 15, count
   end
 
   def stub_time
